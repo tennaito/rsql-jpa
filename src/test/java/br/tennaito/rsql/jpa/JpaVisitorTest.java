ad1ed93acb68fb7522bd2c8b428ea790c6472393
tennaito <tennaito@gmail.com>
2015-02-12 20:37:14 -0200

    Initial version.

---
+++ src/test/java/br/tennaito/rsql/jpa/JpaVisitorTest.java
@@ -0,0 +1,61 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Antonio Rabelo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.tennaito.rsql.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.tennaito.rsql.jpa.entity.Course;
+import cz.jirutka.rsql.parser.RSQLParser;
+import cz.jirutka.rsql.parser.ast.Node;
+import cz.jirutka.rsql.parser.ast.RSQLVisitor;
+
+/**
+ * @author AntonioRabelo
+ */
+public class JpaVisitorTest extends AbstractVisitorTest<Course> {
+
+    @Before
+    public void setUp() throws Exception {
+    	entityManager = EntityManagerFactoryInitializer.getEntityManagerFactory().createEntityManager();
+        entityClass = Course.class;
+    }
+
+    @Test
+    public void testCreateCriterion() throws Exception {
+    	Node rootNode = new RSQLParser().parse("department.id==2");
+    	RSQLVisitor<CriteriaQuery<Course>, EntityManager> visitor = new JpaCriteriaQueryVisitor<Course>();
+    	CriteriaQuery<Course> query = rootNode.accept(visitor, entityManager);
+
+    	List<Course> courses = entityManager.createQuery(query).getResultList();
+
+    	// TODO
+    }
+
+}

2d758b45815dde065433dde18b7532d23e73ce7b
markford <mark@massfords.com>
2017-04-27 05:21:19 -0400

    - added support for comparison of any type of Comparable as opposed to assuming the argument is a Number - added test cases to support the changes - modified the Teacher entity for use in negative tests

--- src/test/java/com/github/tennaito/rsql/jpa/entity/Teacher.java
+++ src/test/java/com/github/tennaito/rsql/jpa/entity/Teacher.java
@@ -17,5 +17,18 @@ public class Teacher extends AbstractTestEntity {
         this.specialtyDescription = specialtyDescription;
     }
 
-
+    /**
+     * Added this method to assist with testing.
+     *
+     * When used with the DefaultArgumentParser, an object requires a valueOf method in order
+     * to be converted from a string.
+     *
+     * @param s name of the teacher
+     * @return Teacher with the name field set but nothing else
+     */
+    public static Teacher valueOf(String s) {
+        Teacher teacher = new Teacher();
+        teacher.setName(s);
+        return teacher;
+    }
 }
